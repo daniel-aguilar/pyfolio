b4255a309f5be6ab7fe5bd49959b77cabd80c1f4
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2017-09-09 22:41:58 -0600

    Add custom template tag 'navlink'
    Adds a custom template tag 'navlink' to set up an active item in a
    Bootstrap navbar, based on a provided url name and the current url.

---
+++ patients/templatetags/nav.py
@@ -0,0 +1,20 @@
+from django import template
+from django.urls import reverse
+from django.utils.html import escape, format_html
+
+register = template.Library()
+
+
+@register.inclusion_tag('nav/navlink.html', takes_context=True)
+def navlink(context, url_name, anchor_text):
+    current_url = context['request'].path
+    nav_url = reverse(url_name)
+
+    is_active = nav_url == current_url
+    li_attr = 'class=active' if is_active else ''
+
+    return {
+        'url': nav_url,
+        'anchor_text': escape(anchor_text),
+        'attr': li_attr
+    }
