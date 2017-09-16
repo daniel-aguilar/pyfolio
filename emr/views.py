72341fcd71a0e6623240fa0d0d6379b5e560c52a
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2017-09-16 15:47:20 -0600

    Add success messages

--- emr/views.py
+++ emr/views.py
@@ -1,6 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse, reverse_lazy
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   UpdateView, View)
 
@@ -20,29 +23,46 @@ class PatientDetail(LoginRequiredMixin, DetailView):
     model = Patient
 
 
-class PatientCreate(LoginRequiredMixin, CreateView):
+class PatientCreate(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Patient
     success_url = reverse_lazy('emr:patient-list')
     fields = '__all__'
     template_name_suffix = '_create_form'
+    success_message = _('Patient %(patient_full_name)s added')
+
+    def get_success_message(self, cleaned_data):
+        patient = self.object
+        return self.success_message % dict(patient_full_name=patient.full_name())
 
 
-class PatientUpdate(LoginRequiredMixin, UpdateView):
+class PatientUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = Patient
-    success_url = reverse_lazy('emr:patient-list')
     fields = '__all__'
     template_name_suffix = '_update_form'
+    success_message = _('Personal information updated')
+
+    def get_success_url(self):
+        patient = self.get_object()
+        return reverse('emr:patient-detail', args=[patient.id])
 
 
 class PatientDelete(LoginRequiredMixin, DeleteView):
     model = Patient
     success_url = reverse_lazy('emr:patient-list')
+    success_message = _('Patient %(patient_full_name)s removed')
+
+    # HACK: DeleteView currently does not support SuccessMessageMixin, so this method had to be overridden
+    def delete(self, request, *args, **kwargs):
+        patient = self.get_object()
+        messages.success(request, self.success_message % dict(patient_full_name=patient.full_name()))
+        return super().delete(self, request, *args, **kwargs)
 
 
 class MedicalRecordCreate(LoginRequiredMixin, View):
     template_name = 'emr/medical_records/create_form.html'
     form_class = MedicalRecordForm
     model = MedicalRecord
+    success_message = _('Medical record created')
 
     def get(self, request, patient_pk):
         patient = get_object_or_404(Patient, pk=patient_pk)
@@ -57,13 +77,14 @@ class MedicalRecordCreate(LoginRequiredMixin, View):
             medical_record.patient = patient
             medical_record.save()
 
+            messages.success(request, self.success_message)
             return redirect('emr:patient-detail', patient.id)
 
         else:
             return render(request, self.template_name, {'form': form, 'patient': patient})
 
 
-class MedicalRecordUpdate(LoginRequiredMixin, UpdateView):
+class MedicalRecordUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = MedicalRecord
     fields = [
         'current_condition',
@@ -75,6 +96,7 @@ class MedicalRecordUpdate(LoginRequiredMixin, UpdateView):
         'additional_notes',
     ]
     template_name = 'emr/medical_records/update_form.html'
+    success_message = _('Medical record updated')
 
     def get_success_url(self):
         medical_record = self.get_object()
