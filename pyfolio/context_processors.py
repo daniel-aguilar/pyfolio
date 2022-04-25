ef9f934277d8ad6d1fb635f88a211bc9e5edbe19
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2022-04-24 22:57:27 -0600

    Add automatic year copyright notice

--- pyfolio/context_processors.py
+++ pyfolio/context_processors.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
 from . import __version__
 
 
 def version(request):
     return {'project_version': __version__}
+
+
+def current_year(request):
+    return {'current_year': datetime.today().strftime('%Y')}
--- pyfolio/settings/production.py
+++ pyfolio/settings/production.py
@@ -47,7 +47,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'pyfolio.context_processors.version'
+                'pyfolio.context_processors.version',
+                'pyfolio.context_processors.current_year',
             ],
         },
     },
