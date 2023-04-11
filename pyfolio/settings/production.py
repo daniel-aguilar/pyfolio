0f1f6bb1bd3e33d81667edaafce9cd5d373efc34
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2023-04-10 19:02:44 -0600

    Remove redundant LocaleMiddleware

--- pyfolio/settings/production.py
+++ pyfolio/settings/production.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'pyfolio.urls'
