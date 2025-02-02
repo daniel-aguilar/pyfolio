b53944ce9829200c2f8af533efc83c1402d88d10
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2025-02-02 12:42:20 -0600

    Upgrade to Bootstrap 5

--- pyfolio/settings/production.py
+++ pyfolio/settings/production.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base_site',
     'emr',
-    'bootstrap4',
+    'django_bootstrap5',
     'fontawesomefree',
     'django_extensions',
     'django_s3_storage',
