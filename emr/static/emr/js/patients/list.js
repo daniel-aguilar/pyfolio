c74a95b100dc0e999af66f15994c83b9db9e5475
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2025-02-02 12:33:35 -0600

    Use Handlebars.js to render options template

--- emr/static/emr/js/patients/list.js
+++ emr/static/emr/js/patients/list.js
@@ -1,62 +1,38 @@
 'use strict';
 
-const data = JSON.parse(document.querySelector('#js-data').textContent);
+function initDataTable() {
+  const jsData = JSON.parse(document.querySelector('#js-data').textContent);
+  const source = document.getElementById('options-template').innerHTML;
+  const template = Handlebars.compile(source);
 
-new DataTable('#patient-list', {
-  language: {
-    url: data.urls.datatablesLanguage
-  },
-  ajax: data.urls.patientList,
-  columns: [
-    { data: 'identification' },
-    { data: 'first_name' },
-    { data: 'last_name' },
-    {
+  new DataTable('#patient-list', {
+    language: {
+      url: jsData.urls.datatablesLanguage
+    },
+    ajax: jsData.urls.patientList,
+    columns: [
+      { data: 'identification' },
+      { data: 'first_name' },
+      { data: 'last_name' },
+      {
         data: 'id',
         orderable: false,
         searchable: false,
-        render: function(id) {
-          // A rather hacky way to render a template
-          const viewURL = `${data.urls.viewPatient + id}/`
-          const updateURL = `${data.urls.viewPatient + id}/`
-          const deleteURL = `${data.urls.deletePatient + id}/`
-          const template =
-            `<div class="options-md">
-              <a class="btn btn-outline-dark" href="${viewURL}"
-                title="${data.lang.view}">
-                <span class="fa-solid fa-eye"</span>
-              </a>
-              <a class="btn btn-outline-dark" href="${updateURL}"
-                  title="${data.lang.edit}">
-                <span class="fa-solid fa-pencil"</span>
-              </a>
-              <a class="btn btn-outline-dark" href="${deleteURL}"
-                  title="${data.lang.delete}">
-                <span class="fa-solid fa-trash-can"</span>
-              </a>
-            </div>
+        render: function (id) {
+          const context = {
+            urls: {
+              view: `${jsData.urls.viewPatient + id}/`,
+              update: `${jsData.urls.updatePatient + id}/`,
+              delete: `${jsData.urls.deletePatient + id}/`,
+            },
+            lang: jsData.lang,
+          };
 
-            <div class="dropdown options-sm">
-              <button class="btn btn-secondary dropdown-toggle" type="button"
-                  data-toggle="dropdown">
-                <span class="fa-solid fa-bars"></span>
-              </button>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="${viewURL}">
-                  ${data.lang.view}
-                </a>
-                <a class="dropdown-item" href="${updateURL}">
-                  ${data.lang.edit}
-                </a>
-                <a class="dropdown-item" href="${deleteURL}">
-                  ${data.lang.delete}
-                </a>
-              </div>
-            </div>`;
-
-            return template;
+          return template(context);
         },
-    },
-  ],
-  orderMulti: false,
-});
+      },
+    ],
+    orderMulti: false,
+  });
+};
+initDataTable();
